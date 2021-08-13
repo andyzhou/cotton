9c5ca04e34b50195348010a618cf11ba6f65a5af
zhou zhirui <zhouozhirui@nibirutech.com>
2021-08-13 12:37:37 +0800

    tool improvement

--- face/tool.go
+++ face/tool.go
@@ -2,13 +2,20 @@ package face
 
 import (
 	"errors"
+	"fmt"
 	"github.com/emicklei/go-restful/v3"
 	"github.com/gorilla/schema"
+	"strings"
 )
 
 /*
  * rest tool face
  */
+
+//inter macro define
+const (
+	HttpProtocol = "://"
+)
 
 //face info
 type Tool struct {
@@ -26,10 +33,7 @@ func NewTool() *Tool {
 }
 
 //parse request form
-func (f *Tool) ParseReqForm(
-				formFace interface{},
-				req *restful.Request,
-			) error {
+func (f *Tool) ParseReqForm(formFace interface{}, req *restful.Request) error {
 	//basic check
 	if formFace == nil || req == nil {
 		return errors.New("invalid parameters")
@@ -47,4 +51,85 @@ func (f *Tool) ParseReqForm(
 		return err
 	}
 	return nil
+}
+
+//get refer domain
+func (f *Tool) GetReferDomain(referUrl string) string {
+	var (
+		referDomain string
+	)
+	if referUrl == "" {
+		return referDomain
+	}
+	//find first '://' pos
+	protocolLen := len(HttpProtocol)
+	protocolPos := strings.Index(referUrl, HttpProtocol)
+	if protocolPos <= -1 {
+		return referDomain
+	}
+	//pick domain
+	tempBytes := []byte(referUrl)
+	tempBytesLen := len(tempBytes)
+	prefixLen := protocolPos + protocolLen
+	resetUrl := tempBytes[prefixLen:tempBytesLen]
+	tempSlice := strings.Split(string(resetUrl), "/")
+	if tempSlice == nil || len(tempSlice) <= 0 {
+		return referDomain
+	}
+	referDomain = fmt.Sprintf("%s%s", tempBytes[0:prefixLen], tempSlice[0])
+	return referDomain
+}
+
+//get all ip from client
+func (f *Tool) GetClientIp(req *restful.Request) []string {
+	var (
+		tempStr string
+		ipSlice = make([]string, 0)
+	)
+
+	//get original data
+	clientAddress := req.Request.RemoteAddr
+	xRealIp := req.Request.Header.Get("X-Real-IP")
+	xForwardedFor := req.Request.Header.Get("X-Forwarded-For")
+
+	//analyze general ip
+	if clientAddress != "" {
+		tempStr = f.analyzeClientIp(clientAddress)
+		if tempStr != "" {
+			ipSlice = append(ipSlice, tempStr)
+		}
+	}
+
+	//analyze x-real-ip
+	if xRealIp != "" {
+		tempStr = f.analyzeClientIp(clientAddress)
+		if tempStr != "" {
+			ipSlice = append(ipSlice, tempStr)
+		}
+	}
+
+	//analyze x-forward-for
+	//like:192.168.0.1,192.168.0.2
+	if xForwardedFor != "" {
+		tempSlice := strings.Split(xForwardedFor, ",")
+		if len(tempSlice) > 0 {
+			for _, tmpAddr := range tempSlice {
+				tempStr = f.analyzeClientIp(tmpAddr)
+				if tempStr != "" {
+					ipSlice = append(ipSlice, tempStr)
+				}
+			}
+		}
+	}
+
+	return ipSlice
+}
+
+//analyze client ip
+func (f *Tool) analyzeClientIp(address string) string {
+	tempSlice := strings.Split(address, ":")
+	if len(tempSlice) < 1 {
+		return ""
+	}
+	return tempSlice[0]
 }
--- iface/tool.go
+++ iface/tool.go
@@ -4,8 +4,7 @@ import "github.com/emicklei/go-restful/v3"
 
 //rest tool interface
 type ITool interface {
-	ParseReqForm(
-		formFace interface{},
-		req *restful.Request,
-	) error
+	ParseReqForm(formFace interface{}, req *restful.Request) error
+	GetReferDomain(referUrl string) string
+	GetClientIp(req *restful.Request) []string
 }
